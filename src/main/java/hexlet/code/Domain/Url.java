14953b22aec221bfacc1d61cd25ad37d137d6981
Vladislav.P <vladppk@protonmail.com>
2022-09-27 19:05:32 +0200

    add model

--- src/main/java/hexlet/code/Domain/Url.java
+++ src/main/java/hexlet/code/Domain/Url.java
@@ -17,4 +17,28 @@ public class Url extends Model {
     @WhenCreated
     private String createdAt;
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
 }
