96d9e22de69936260d6f411ac1df8a5b85500c32
Vladislav.P <vladppk@protonmail.com>
2022-10-09 00:53:39 +0200

    add controllers

--- src/main/java/hexlet/code/Controllers/UrlController.java
+++ src/main/java/hexlet/code/Controllers/UrlController.java
@@ -1,9 +1,91 @@
 package hexlet.code.Controllers;
 
+import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
+import io.ebean.PagedList;
 import io.javalin.http.Handler;
+import io.javalin.http.NotFoundResponse;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Objects.requireNonNull;
 
 public final class UrlController {
-    public static Handler wtf = ctx -> {
-        ctx.render("main.html");
+    public static Handler addUrl = ctx -> {
+        // get input as url
+        String formParam = ctx.formParam("url");
+        URL formedUrl = new URL(formParam);
+        String validUrl = formedUrl.getProtocol() + "://" + formedUrl.getAuthority();
+
+        if (formedUrl == null) {
+            ctx.sessionAttribute("flash", "Некорректный URL");
+            ctx.sessionAttribute("flash-type", "danger");
+            ctx.redirect("/");
+            return;
+        }
+
+        Url url = new QUrl()
+                .name.equalTo(validUrl)
+                .findOne();
+        if (url != null) {
+            ctx.sessionAttribute("flash", "Страница уже существует");
+            ctx.sessionAttribute("flash-type", "info");
+            ctx.redirect("/urls");
+            return;
+        }
+
+        Url newUrl = new Url(validUrl);
+        newUrl.save();
+        ctx.sessionAttribute("flash", "Страница успешно добавлена");
+        ctx.sessionAttribute("flash-type", "success");
+        ctx.redirect("/urls");
+        return;
+    };
+
+    public static Handler showUrls = ctx -> {
+      // get all urs out of db and list them
+        int page = ctx.queryParamAsClass("page", Integer.class).getOrDefault(1) - 1;
+        int rowsPerPage = 10;
+        PagedList<Url> pagedUrls = new QUrl()
+                .setFirstRow(page * rowsPerPage)
+                .setMaxRows(rowsPerPage)
+                .orderBy()
+                .id.asc()
+                .findPagedList();
+        List<Url> urls = pagedUrls.getList();
+        int lastPage = pagedUrls.getTotalPageCount() + 1;
+        int currentPage = pagedUrls.getPageIndex() + 1;
+        List<Integer> pages = IntStream
+                .range(1, lastPage)
+                .boxed()
+                .collect(Collectors.toList());
+        ctx.attribute("urls", urls);
+        ctx.attribute("pages", pages);
+        ctx.attribute("currentPage", currentPage);
+        ctx.render("listUrls.html");
+    };
+
+    public static Handler showUrl = ctx -> {
+        // show the exact url by urls/{id}
+        int id = ctx.pathParamAsClass("id", Integer.class).getOrDefault(null);
+
+        Url url = new QUrl()
+                .id.equalTo(id)
+                .findOne();
+        if (url == null) {
+            throw new NotFoundResponse();
+        }
+        ctx.attribute("url", url);
+        ctx.render("showUrl.html");
+
+    };
+
+    public static Handler addCheck = ctx -> {
+      ctx.render("main.html");
     };
 }
