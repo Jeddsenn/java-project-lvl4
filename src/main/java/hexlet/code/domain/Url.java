beed53816873303ab14546f1c3d1e697f7bcd27f
Vladislav.P <vladppk@protonmail.com>
2022-10-09 18:05:20 +0200

    update model

--- src/main/java/hexlet/code/domain/Url.java
+++ src/main/java/hexlet/code/domain/Url.java
@@ -1,22 +1,31 @@
-package hexlet.code.Domain;
+package hexlet.code.domain;
 
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.List;
 
 @Entity
 public class Url extends Model {
 
     @Id
+    @GeneratedValue
     private long id;
 
     private String name;
 
     @WhenCreated
-    private String createdAt;
+    private Instant createdAt;
 
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<UrlCheck> urlChecks;
+
+
+    public Url(String name) {
+        this.name = name;
+    }
 
     public long getId() {
         return id;
@@ -34,11 +43,8 @@ public class Url extends Model {
         this.name = name;
     }
 
-    public String getCreatedAt() {
+    public Instant getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
 }
