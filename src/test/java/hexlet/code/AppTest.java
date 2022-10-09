b9bafda7ef44eed701eef063780750d2054a0d82
Vladislav.P <vladppk@protonmail.com>
2022-10-09 17:22:52 +0200

    add some tests for url controller

--- src/test/java/hexlet/code/AppTest.java
+++ src/test/java/hexlet/code/AppTest.java
@@ -1,20 +1,20 @@
 package hexlet.code;
 
 import hexlet.code.domain.Url;
+import hexlet.code.domain.query.QUrl;
 import io.ebean.DB;
 import io.ebean.Database;
 import io.javalin.Javalin;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.junit.jupiter.api.Assertions.*;
+import kong.unirest.HttpResponse;
 
 class AppTest {
 
     private static Javalin app;
     private static String baseUrl;
+    private static Url existingUrl;
     private static Database database;
 
 
@@ -32,12 +32,10 @@ class AppTest {
         app.stop();
     }
 
-    @Test
-    void main() {
-    }
-
-    @Test
-    void getApp() {
+    @BeforeEach
+    void beforeEach() {
+        database.script().run("/truncate.sql");
+        database.script().run("/seed-test-db.sql");
     }
 
     @Test
@@ -45,4 +43,62 @@ class AppTest {
         assertThat(true).isEqualTo(true);
     }
 
+    @Nested
+    class RootTest {
+        @Test
+        void testMain() {
+            HttpResponse<String> response = Unirest.get(baseUrl).asString();
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).contains("Анализатор страниц");
+        }
+    }
+
+    @Nested
+    class UrlTest {
+
+        @Test
+        void testAddUrl() {
+            String inputUrl = "https://www.cia.edu";
+            HttpResponse<String> responsePost = Unirest
+                    .post(baseUrl + "/urls")
+                    .field("url", inputUrl)
+                    .asEmpty();
+
+            assertThat(responsePost.getStatus()).isEqualTo(302);
+
+            Url dbUrl = new QUrl()
+                    .name.equalTo(inputUrl)
+                    .findOne();
+
+            assertThat(dbUrl).isNotNull();
+            assertThat(dbUrl.getName()).isEqualTo(inputUrl);
+        }
+
+        @Test
+        void testListUrls() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("https://www.example.com");
+            assertThat(body).contains("https://www.hexlet.io");
+            assertThat(body).contains("https://www.google.ru");
+        }
+
+        @Test
+        void testShowUrl() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/1")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("https://www.example.com");
+        }
+
+
+
+    }
 }
