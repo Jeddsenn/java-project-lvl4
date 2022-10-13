dedfb3cc6314d9c4ea934d2f98800eb54ba258f8
vmj <theecrapdump@gmail.com>
2022-10-13 15:33:38 +0200

    refactor project structure

--- src/test/resources/seed-test-db.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO URL
-  (name, created_at)
-VALUES
-  ('https://www.example.com', CURRENT_TIMESTAMP),
-  ('https://www.hexlet.io', CURRENT_TIMESTAMP),
-  ('https://www.google.ru', CURRENT_TIMESTAMP);
